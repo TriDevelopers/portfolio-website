e31522facf5e1733d1f294e40e9913f2b65dbace
TriDevelopers <ndmt.2503@gmail.com>
2023-06-14 21:00:19 -0500

    Update contact/resume

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Tri Ngo</h1>
+        <h5 classname="text-light">Software Engineer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
