e621be246ea0766db7643449cb2238ab5a087b9f
TriDevelopers <ndmt.2503@gmail.com>
2023-06-17 13:44:12 -0500

    Finish Navigation Bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Tri Ngo</h1>
-        <h5 className="text-light">Software Engineer</h5>
+        <h5 className="text-light">Computer Science Student</h5>
         <CTA />
         <HeaderSocials />
         
@@ -18,7 +18,7 @@ const Header = () => {
           <img src={ME} alt="me" />
         </div>
 
-        <a href="#contact" className='scroll__down'>
+        <a href="#contact" className='scroll__down'> {/* Go straight to contact when click */}
           Scroll Down
         </a>
       </div>
