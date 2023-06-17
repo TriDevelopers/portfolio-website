5be4484530563c8f3fa63574a44124c0d65f6a82
TriDevelopers <ndmt.2503@gmail.com>
2023-06-17 18:03:42 -0500

    Finish Services section

--- src/components/service/Services.jsx
+++ src/components/service/Services.jsx
@@ -1,9 +1,79 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="services">
+          <div className="services__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="services__list">
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Paragraph 1</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Paragraph 2</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Paragraph 3</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of UI/UX services */}
+        <article className="services">
+          <div className="services__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="services__list">
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Paragraph 1</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Paragraph 2</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Paragraph 3</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Web services */}
+        <article className="services">
+          <div className="services__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="services__list">
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Paragraph 1</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Paragraph 2</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Paragraph 3</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Content services */}
+      </div>
+    </section>
   )
 }
 
