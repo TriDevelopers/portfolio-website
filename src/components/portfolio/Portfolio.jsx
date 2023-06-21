4fb3884fbc079b840e29c86f2aaf88ac716ae312
TriDevelopers <ndmt.2503@gmail.com>
2023-06-21 17:22:22 -0500

    Portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+// DO NOT USE IMAGE IN PRODUCTION
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'This is a portfolio item title 1',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'This is a portfolio item title 2',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'This is a portfolio item title 3',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'This is a portfolio item title 4',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'This is a portfolio item title 5',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'This is a portfolio item title 6',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio-container">
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className="portfolio__item">
+              <div className="portfolio__item-img">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target='_blank'>GitHub</a>
+                <a href={demo} className="btn btn-primary" target='_blank'>Live Demo</a>
+              </div>
+            </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
