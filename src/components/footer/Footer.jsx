5e8033ee28e5ac3b705bba91077d456fb679ac98
TriDevelopers <ndmt.2503@gmail.com>
2023-07-05 11:58:48 -0500

    Finish

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin, BsGithub} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">TRI NGO</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/tridinhminhngo/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/TriDevelopers" target="_blank"><BsGithub/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR All right resevered</small>
+      </div>
+    </footer>
   )
 }
 
