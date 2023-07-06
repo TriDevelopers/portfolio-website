96dc9b10c201c7093c49bf2565f19226d7c50f6d
TriDevelopers <ndmt.2503@gmail.com>
2023-07-06 11:11:34 -0500

    Fix

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './testimonials.css'
-import AVTR1 from '../../assets/avatar1.jpg'
-import AVTR2 from '../../assets/avatar2.jpg'
-import AVTR3 from '../../assets/avatar3.jpg'
-import AVTR4 from '../../assets/avatar4.jpg'
+import LOGO1 from '../../assets/DataFestLogoSMU.png'
+import LOGO2 from '../../assets/J.P.-Morgan-Chase-Logo.png'
+import LOGO3 from '../../assets/CodePath-1.png'
+import {FaAward} from 'react-icons/fa'
 
 // import Swiper core and required modules
 import { Pagination } from 'swiper';
@@ -15,24 +15,22 @@ import "swiper/css/pagination";
 
 const data = [
   {
-    avatar: AVTR1,
-    name: 'Name1',
-    review: 'Review 1',
+    avatar: LOGO1,
+    name: 'ASA DataFest 2022',
+    review: 'Ranked 1st in the competition among undergraduate teams from various universities competed over a long weekend to come up with the most compelling analysis of a large, real-world data set',
+    link: 'https://www.linkedin.com/posts/danny-reyes-mendez_our-firms-culture-is-founded-on-the-commitment-activity-6937478515105685504-kpJZ?utm_source=share&utm_medium=member_desktop'
   },
   {
-    avatar: AVTR2,
-    name: 'Name2',
-    review: 'Review 2',
+    avatar: LOGO2,
+    name: 'Software Engineer Virtual Experience',
+    review: 'Interfaced with a stock price data feed to monitor stock price. Used JPMorgan Chase & Co. frameworks and tools to display data visually for traders.',
+    link: 'https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/J.P.%20Morgan/R5iK7HMxJGBgaSbvk_J.P.%20Morgan_vBn3c9MEh7yohz9P9_1682276195278_completion_certificate.pdf'
   },
   {
-    avatar: AVTR3,
-    name: 'Name3',
-    review: 'Review 3',
-  },
-  {
-    avatar: AVTR4,
-    name: 'Name4',
-    review: 'Review 4',
+    avatar: LOGO3,
+    name: 'Intro to CyberSecurity',
+    review: 'Learnt about topics such as steganography, networking protocols, cracking passwords, virus detection, and more, utilizing tools including Metasploit, Kali linux, and Shodan.',
+    link: 'https://drive.google.com/file/d/1ePbxb3Jsjqvd7t7k6kvP4IokvBtw0JDQ/view?pli=1'
   }
 ]
 
@@ -52,13 +50,13 @@ const Testimonials = () => {
       slidesPerView={1}
       >
         {
-          data.map(({ avatar, name, review }, index) => {
+          data.map(({ avatar, name, review, link }, index) => {
             return (
               <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} />
               </div>
-              <h5 className="client__name">{name}</h5>
+              <h5 className="client__name">{name} <a href={link}><FaAward/></a></h5>
               <small className="client__review">
                 {review}
               </small>
