e8e1001daf308af3f6201d37cc979317f4a1490f
TriDevelopers <ndmt.2503@gmail.com>
2023-08-14 17:59:58 -0500

    Update experience

--- src/components/service/Services.jsx
+++ src/components/service/Services.jsx
@@ -11,28 +11,6 @@ const Services = () => {
       <div className="container services__container">
         <article className="services">
           <div className="services__head">
-            <h3>Software Engineer - Volunteer</h3>
-            <h3><a href="https://www.linkedin.com/company/avolta-inc/" target="_blank">@ Avolta Inc.</a></h3>
-          </div>
-
-          <ul className="services__list">
-            <li>
-              <BiCheck className='services__list-icon' />
-              <p>Paragraph 1</p>
-            </li>
-            <li>
-              <BiCheck className='services__list-icon' />
-              <p>Paragraph 2</p>
-            </li>
-            <li>
-              <BiCheck className='services__list-icon' />
-              <p>Paragraph 3</p>
-            </li>
-          </ul>
-        </article>
-        {/* End of UI/UX services */}
-        <article className="services">
-          <div className="services__head">
             <h3>Software Engineer - Intern</h3>
             <h3><a href="https://www.linkedin.com/company/svtechnologiesjsc/" target="_blank">@ SVTech</a></h3>
           </div>
@@ -40,41 +18,57 @@ const Services = () => {
           <ul className="services__list">
             <li>
               <BiCheck className='services__list-icon' />
-              <p>Paragraph 1</p>
+              <p>Developed a RESTful API application with Java and Spring Boot</p>
             </li>
             <li>
               <BiCheck className='services__list-icon' />
-              <p>Paragraph 2</p>
+              <p>Analyzed and integrated data from multiple 3rd party APIs</p>
             </li>
             <li>
               <BiCheck className='services__list-icon' />
-              <p>Paragraph 3</p>
+              <p>Deployed the application onto an Ubuntu server</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Improved the accuracy of the facial recognition system by 20%</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Allowed for seamless integration and boosted team performance by 30%</p>
             </li>
           </ul>
         </article>
-        {/* End of Web services */}
+        {/* End of SVTech */}
         <article className="services">
           <div className="services__head">
-            <h3>Tech Fellow</h3>
-            <h3><a href="https://www.linkedin.com/school/codepath-org/" target="_blank">@ CodePath</a></h3>
+            <h3>Software Engineer - Volunteer</h3>
+            <h3><a href="https://www.linkedin.com/company/avolta-inc/" target="_blank">@ Avolta Inc.</a></h3>
           </div>
 
           <ul className="services__list">
             <li>
               <BiCheck className='services__list-icon' />
-              <p>Paragraph 1</p>
+              <p>Developed multiple API endpoints with Python and Flask</p>
             </li>
             <li>
               <BiCheck className='services__list-icon' />
-              <p>Paragraph 2</p>
+              <p>Developed functions to authenticate and authorize user</p>
             </li>
             <li>
               <BiCheck className='services__list-icon' />
-              <p>Paragraph 3</p>
+              <p>Implemented parameterized queries to avoid SQL Injection</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Improved the security of database queries function</p>
+            </li>
+            <li>
+              <BiCheck className='services__list-icon' />
+              <p>Currently developing a GPS system for the application</p>
             </li>
           </ul>
         </article>
-        {/* End of Content services */}
+        {/* End of Avolta */}
       </div>
     </section>
   )
